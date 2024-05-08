551dd94735e24a5eb46e420e55a51fbf99f450c5
MalaikaTariq726 <155721070+MalaikaTariq726@users.noreply.github.com>
2024-05-09 02:21:48 +0500

    implementing forgot password functionality

---
+++ client/app/src/user_management/ForgotPassword.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../CSS/forgotPassword.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+import { useUser } from "../services/userService";
+
+const ForgotPassword = () => {
+  const navigate = useNavigate();
+  const { forgotPassword } = useUser();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
+  const [passwordsMatch, setPasswordsMatch] = useState(true);
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleConfirmPasswordChange = (e) => {
+    const confirmPasswordValue = e.target.value;
+    setConfirmPassword(confirmPasswordValue);
+    setPasswordsMatch(confirmPasswordValue === password);
+  };
+
+  const handleForgotPassword = () => {
+    if (email && password && confirmPassword) {
+      if (password === confirmPassword) {
+        forgotPassword({ password, email });
+        navigate("/login");
+      } else {
+        setPasswordsMatch(false);
+      }
+    } else {
+      alert("Please fill in all fields.");
+    }
+  };
+
+  return (
+    <>
+      <div className="forgotpasscontainer" id="forgotpasscontainer">
+        <form action="#" className="forgotpassSelectForm">
+          <>
+            <h1 className="forgotpassheading">Forgot Password</h1>
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <input
+              type="text"
+              placeholder="Password"
+              value={password}
+              onChange={handlePasswordChange}
+              required
+            />
+            <input
+              type="text"
+              placeholder="Confirm Password"
+              value={confirmPassword}
+              onChange={handleConfirmPasswordChange}
+              required
+            />
+            <span className="passwordMismatch">{passwordsMatch ? null : "Passwords do not match"}</span>
+            <button
+              className="setForgotpassBtn"
+              id="signUpBtn"
+              onClick={handleForgotPassword}
+            >
+              Done
+            </button>
+          </>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default ForgotPassword;
